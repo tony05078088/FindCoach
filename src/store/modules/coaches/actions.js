55a50552c5b4584153d5fff064a6bad7c251f299
William <tony05078088@gmail.com>
2021-03-17 01:06:21 +0800

    sending request to firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,31 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
     };
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(
+      `https://findcoach-a44ed-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
+    console.log(response);
+    // const responseData = await response.json();
+    // console.log(responseData);
+
+    if (!response.ok) {
+      // error happen
+    }
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 };
