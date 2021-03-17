94ceef5fe5d5ef1a0d40d364c104a2db17a8700e
William <tony05078088@gmail.com>
2021-03-17 11:17:09 +0800

    fetch data from firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,5 +27,30 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://findcoach-a44ed-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+    if (!response.ok) {
+      // error ...
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+      coaches.push(coach);
+      console.log(coaches);
+    }
+    context.commit('setCoaches', coaches);
   }
 };
--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -2,5 +2,8 @@ export default {
   registerCoach(state, payload) {
     state.coaches.push(payload);
     //alert('Create Successfully!');
+  },
+  setCoaches(state, payload) {
+    state.coaches = payload;
   }
 };
