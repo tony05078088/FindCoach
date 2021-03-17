20388c2f2bfa1d93b40098dd6989523c8d27b8c7
William <tony05078088@gmail.com>
2021-03-17 18:01:40 +0800

    fetch data from firebase

--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -1,7 +1,10 @@
 export default {
   addRequest(state, payload) {
-                               state.requests.push(payload);
-                               // console.log(payload);
-                               // console.log(state.requests);
-                             }
+    state.requests.push(payload);
+    // console.log(payload);
+    // console.log(state.requests);
+  },
+  setRequest(state, payload) {
+    state.requests = payload;
+  }
 };
